6a71b8c741beb173481c062972b4f97c09f02856
zhonghcc <zhonghcc@gmail.com>
2019-02-13 22:52:33 +0800

    zevms max user

--- src/main/java/com/zhonghcc/agent/dump/ClassDumpAgent.java
+++ src/main/java/com/zhonghcc/agent/dump/ClassDumpAgent.java
@@ -26,21 +26,53 @@ public class ClassDumpAgent implements ClassFileTransformer {
 //        this.add("a.本地数据库");
 //        this.add("abc.Game");
 //        this.add("server.ServerProperties");
-        this.add("handling.login.LoginServer");
-        this.add("handling.login.LoginWorker");
-        this.add("handling.login.handler.CharLoginHandler");
-        this.add("handling.login.Balloon");
+//        this.add("handling.login.LoginServer");
+//        this.add("handling.login.LoginWorker");
+//        this.add("handling.login.handler.CharLoginHandler");
+//        this.add("handling.login.Balloon");
+//        this.add("a");
+//        this.add("abc");
+//        this.add("aVer.2");
+//        this.add("client");
+//
+//        this.add("constants");
+//        this.add("data");
+//        this.add("database");
+//        this.add("database1");
+//        this.add("download");
+//        this.add("folder");
+//        this.add("fumo");
+//        this.add("gui");
+//        this.add("handling");
+//        this.add("properties");
+//        this.add("provider");
+//        this.add("pvp");
+//        this.add("quest");
+//        this.add("scripting");
+//        this.add("server");
+//        this.add("tools");
+//        this.add("zevms");
+        this.add("handling.world.MapleParty");
+
     }};
     public byte[] transform(ClassLoader loader, final String className, final Class<?> classBeingRedefined, ProtectionDomain protectionDomain, final byte[] classfileBuffer) throws IllegalClassFormatException {
 
         String classNamePot = className.replace('/','.');
-        if(!classNames.contains(classNamePot)){
+
+        if(!classNames.contains(className.split("/")[0])&&
+                !classNames.contains(classNamePot)){
             return null;
         }
+
 //        startDump(className,classBeingRedefined,classfileBuffer);
         new Thread(new Runnable() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(30000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 startDump(className,classBeingRedefined,classfileBuffer);
             }
         }).start();
@@ -48,15 +80,11 @@ public class ClassDumpAgent implements ClassFileTransformer {
         return null;
     }
 
-    public void startDump(String className,Class<?> classBeingRedefined, byte[] classfileBuffer){
-        try {
-            Thread.sleep(30000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public synchronized void startDump(String className,Class<?> classBeingRedefined, byte[] classfileBuffer){
+
         String filePath = "/tmp/" + className.replace('.', '/') + ".class";
         String classNamePot = className.replace('/','.');
-        System.out.println("dump "+classNamePot + " to "+filePath + " size="+classfileBuffer.length);
+//        System.out.println("dump "+classNamePot + " to "+filePath + " size="+classfileBuffer.length);
         try {
 
 
@@ -69,22 +97,56 @@ public class ClassDumpAgent implements ClassFileTransformer {
             Class cc = Class.forName(classNamePot);
             Field[] fields = Class.forName(classNamePot).getDeclaredFields();
             Method[] methods = cc.getDeclaredMethods();
-            System.out.println("CLASS: "+classNamePot);
-            System.out.println("field-------");
+//            System.out.println("CLASS: "+classNamePot);
+//            System.out.println("field-------");
             Object instance = null;
             for(Field field : fields){
-                System.out.println(field.getName() + ":" +field.getType().getName()+ " static:" + Modifier.isStatic(field.getModifiers()));
+//                System.out.println(field.getName() + ":" +field.getType().getName()+ " static:" + Modifier.isStatic(field.getModifiers()));
 
                 if(Modifier.isStatic(field.getModifiers())){
                     field.setAccessible(true);
                     Object f = field.get(Object.class);
-                    System.out.println(field.getName() +" value="+f);
+//                    if (f.toString().equals("1")) {
+                        System.out.println("class="+classNamePot+" field=" + field.getName()+
+                                ":" +field.getType().getName()+ " static:" + " value=" + f);
+//                        field.set(Object.class,4);
+//                    }
 
                 }
-                if(field.getName().equals("instance")){
+                if(field.getName().equals("服务端启动模式")){
                     field.setAccessible(true);
-                    final Object loginInstance = field.get(Object.class);
-                    System.out.println(loginInstance);
+
+                    field.set(Object.class,1);
+                    instance = field.get(Object.class);
+                    System.out.println(field.getName() +" value="+instance);
+                }
+                if(field.getName().equals("容纳人数")){
+                    field.setAccessible(true);
+
+                    field.set(Object.class,40);
+                    final Field fl = field;
+                    instance = field.get(Object.class);
+                    System.out.println(field.getName() +" value="+instance);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(10000);
+                                Object f = fl.get(Object.class);
+                                System.out.println(fl.getName() +" value="+f);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }).start();
+                }
+
+
+//                if(field.getName().equals("instance")){
+//                    field.setAccessible(true);
+//                    final Object loginInstance = field.get(Object.class);
+//                    System.out.println(loginInstance);
 //                    final Field userLimitField = cc.getDeclaredField("userLimit");
 //                    userLimitField.setAccessible(true);
 //
@@ -111,41 +173,41 @@ public class ClassDumpAgent implements ClassFileTransformer {
 
                 }
 
-                if(field.getName().equals("显示人数")||field.getName().equals("显示人数2")){
-                    field.setAccessible(true);
-                    Object f = field.get(instance);
-                    System.out.println(f);
-                }
-                if(field.getName().equals("props")){
-                    field.setAccessible(true);
-                    Object f = field.get(Object.class);
-                    Properties props = (Properties)f;
-                    props.list(System.out);
-                }
-                if(field.getName().equals("单机人数")){
-                    field.setAccessible(true);
-                    Object f = field.get(Object.class);
-                    field.set(Object.class,4);
-                    instance = field.get(Object.class);
-                    System.out.println(field.getName() +" value="+instance);
-                }
-                if(field.getName().equals("调试")||field.getName().equals("调试输出")){
-                    field.setAccessible(true);
-                    Object f = field.get(Object.class);
-                    field.set(Object.class,"开");
-                    instance = field.get(Object.class);
-                    System.out.println(field.getName() +" value="+instance);
-                }
-            }
-            System.out.println("method-------");
-            for(Method method: methods){
-                System.out.println(method.getName());
-                if(method.getName().equals("getInstance")){
-//                    Method getInstance = cc.getClass().getMethod("getInstance",null);
-//                    Object instance = getInstance.invoke(null);
-//                    System.out.println(instance);
-                }
-            }
+//                if(field.getName().equals("显示人数")||field.getName().equals("显示人数2")){
+//                    field.setAccessible(true);
+//                    Object f = field.get(instance);
+//                    System.out.println(f);
+//                }
+//                if(field.getName().equals("props")){
+//                    field.setAccessible(true);
+//                    Object f = field.get(Object.class);
+//                    Properties props = (Properties)f;
+//                    props.list(System.out);
+//                }
+//                if(field.getName().equals("单机人数")){
+//                    field.setAccessible(true);
+//                    Object f = field.get(Object.class);
+//                    field.set(Object.class,4);
+//                    instance = field.get(Object.class);
+//                    System.out.println(field.getName() +" value="+instance);
+//                }
+//                if(field.getName().equals("调试")||field.getName().equals("调试输出")){
+//                    field.setAccessible(true);
+//                    Object f = field.get(Object.class);
+//                    field.set(Object.class,"开");
+//                    instance = field.get(Object.class);
+//                    System.out.println(field.getName() +" value="+instance);
+//                }
+//            }
+//            System.out.println("method-------");
+//            for(Method method: methods){
+//                System.out.println(method.getName());
+//                if(method.getName().equals("getInstance")){
+////                    Method getInstance = cc.getClass().getMethod("getInstance",null);
+////                    Object instance = getInstance.invoke(null);
+////                    System.out.println(instance);
+//                }
+//            }
 
 //            byte[] cryptedByte = cc.toBytecode();
 //
@@ -158,7 +220,7 @@ public class ClassDumpAgent implements ClassFileTransformer {
 //            }
 //            is.close();
 //            out.close();
-            System.out.println("dump "+ className + " complete");
+//            System.out.println("dump "+ className + " complete");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
